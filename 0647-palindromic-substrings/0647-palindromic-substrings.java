8b01ec7b2bce14d0d1b10d7dc05aca85cf27e0bb
Amit-Prajapat <160996847+Amit-Prajapat@users.noreply.github.com>
2026-04-15 01:28:56 +0530

    Time: 1240 ms (5.02%), Space: 53.2 MB (5.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,16 +1,23 @@
 class Solution {
-    public static boolean check(String s,int i,int j){
-        if(i>j) return true;
-        if(s.charAt(i)==s.charAt(j)) return check(s,i+1,j-1);
-        return false;
-
+    //0->false, 1->true
+    public int check(int i,int j,String s,int[][] dp){
+        if(i>j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)) return check(i+1,j-1,s,dp);
+        return 0;
     }
     public int countSubstrings(String s) {
         int count = 0;
         int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        }
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(check(s,i,j)) count++;
+                if(check(i,j,s,dp)==1) count++;
             }
         }
         return count;
